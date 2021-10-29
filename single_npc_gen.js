fa113fbd39b2da1dbf4e129b89f6668f76a07733
Anthony <afrock@uncc.edu>
2021-10-29 17:40:16 -0400

    Internal comments in single_npc_gen.js

--- single_npc_gen.js
+++ single_npc_gen.js
@@ -1,5 +1,5 @@
 // Global list of usable API calls for drop down selection - Anthony
-let dropDownList = ['alignments', 'races', 'classes'];
+let dropDownList = ['alignments', 'races', "classes"];
 let dropDownListAll = ['alignments', 'races', 'gender', 'classes', 'level', 'threat'];
 
 // Initial JSON object creation. I still don't really know how these work. Like classes? idk - Anthony
@@ -17,10 +17,17 @@ let npc = {
     Dynamically generates options for Alignment, Race, and Class from the DnD5e API - Anthony
  */
 for (let i = 0; i < dropDownList.length; i++) {
-    fetch('https://www.dnd5eapi.co/api/' + dropDownList[i])
+    addDropDownApiInfo(dropDownList[i]);
+}
+
+/*
+    Seperated this  from above to be used later and not just at the loading of the page - Anthony
+ */
+function addDropDownApiInfo(selectName) {
+    fetch('https://www.dnd5eapi.co/api/' + selectName)
         .then( response => response.json() )
         .then (data => {
-            let select = document.getElementById(dropDownList[i]);
+            let select = document.getElementById(selectName);
 
             for (const option of data.results) {
                 let opt = document.createElement("option");
@@ -61,7 +68,6 @@ function generateNPC() {
                 break;
         }
     }
-
     document.getElementById("output").innerHTML = printNPC() + "<br/>";
 }
 
@@ -80,20 +86,22 @@ function printNPC() {
     )
 }
 
+
 /*
     To be used to read from csv files and convert to JSON objects for use.
     Not sure if this is the actual function I want I haven't tested it yet - Anthony
  */
 function csvJSON(csv){
-    var lines=csv.split("\n");
-    var result = [];
-    var headers=lines[0].split(",");
+    let lines=csv.split("\n");
+    let result = [];
+    let headers=lines[0].split(",");
 
-    for(var i=1;i<lines.length;i++){
-        var obj = {};
-        var currentline=lines[i].split(",");
+    for(let i=1;i<lines.length;i++){
+        let obj = {};
+        let currentline=lines[i].split(",");
+        let headers = lines[0].split(",");
 
-        for(var j=0;j<headers.length;j++){
+        for(let j=0;j<headers.length;j++){
             obj[headers[j]] = currentline[j];
         }
         result.push(obj);
@@ -101,3 +109,46 @@ function csvJSON(csv){
     //return result; //JavaScript object
     return JSON.stringify(result); //JSON
 }
+
+/*
+        _____________________________________________________
+        | LEAVE THIS COMMENTED OUT IF YOU AREN'T            |
+        | WORKING ON A HTTP HOSTED WEPBAGE                  |
+        | fetch ON A FILE IN OUR REPOSITORY WILL BREAK IT   |
+        -----------------------------------------------------
+
+    Proof of concept to read from JSON files
+    fetch('./Data/first-names.json')
+    .then(data => data.json())
+    .then(data => {
+        document.getElementById("output").innerHTML += data[5] + "<br/>";
+    });
+ */
+
+/*
+*********************
+Work in progress to make Class and Level not show unless Threat is set to Yes - Anthony
+*********************
+
+function showExtras(selectedObject) {
+    let threat = selectedObject.value;
+    let parentDiv = document.getElementById("filters");
+    if (threat === "Yes") {
+        document.getElementById("gen")
+
+        // Dynamically create class label if Threat set to Yes
+        let label = document.createElement("label");
+        label.setAttribute("for", "classes");
+        label.innerHTML = "Class";
+        let select = document.createElement("select");
+        select.id = "classes";
+        select.name = "classes";
+        let opt = document.createElement("option");
+        opt.text = "Random";
+        opt.value = "Random";
+        select.appendChild(opt);
+        label.appendChild(select);
+        parentDiv.appendChild(label);
+    }
+}
+ */
